856994253ebcef1e18b9edc54a5316778ac03c31
timsoxjer <chirikirin.tm@gmail.com>
2025-01-29 14:06:28 +0700

    first commit

---
+++ bookstore/books/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    published_date = models.DateField()
+
+    def __str__(self):
+        return self.title
